3ceec906c4fdd681e29bd9c56ae8b07a10afa330
Gabriel Moreira <moreira.gabrielsoares@gmail.com>
2022-08-05 15:31:54 -0300

    change the method of database connection

--- src/databases/postgreSQL.js
+++ src/databases/postgreSQL.js
@@ -6,11 +6,7 @@ dotenv.config();
 const { Pool } = pg;
 
 const db = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
